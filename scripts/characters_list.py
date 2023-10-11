fafa404944bf958761c5487236ac8bf9cf2932e2
Giao Vu Dinh <tvudinh20@gmail.com>
2023-10-11 17:39:08 -0400

    generated data files in src folder

--- scripts/characters_list.py
+++ scripts/characters_list.py
@@ -7,6 +7,9 @@ import sys
 path_to = os.path.join(os.getcwd(), "data", sys.argv[1])
 path_from = os.path.join(path_to, "chapters")
 
+output_path = os.path.join(os.getcwd(), "src/data", sys.argv[1])
+input_path = os.path.join(output_path, "chapters")
+
 tags = ["intro", "battle", "end", "visit", "flashback", "recruit-visit", "recruit-battle", "character-falls", "dialogue", "boss", "recruit-talk", "battle-talk"]
 
 gender_info = {}
@@ -19,7 +22,7 @@ with open(os.path.join(os.getcwd(), path_to, "genders.json"), 'r') as f:
     gender_info = json.load(f)
 
 # reading JSON
-for filename in glob.glob(os.path.join(path_from, "*.json")):
+for filename in glob.glob(os.path.join(input_path, "*.json")):
     with open(os.path.join(os.getcwd(), filename), 'r') as f:
         data = json.load(f)
 
@@ -35,7 +38,7 @@ for filename in glob.glob(os.path.join(path_from, "*.json")):
                 file_content["counts"][person] += data["dialogue_counts"][person]
 
 # sorting and writing speaker counts
-with open(os.path.join(os.getcwd(), path_to, "speakers.json"), 'w') as file:
+with open(os.path.join(os.getcwd(), output_path, "speakers.json"), 'w') as file:
     sorted_counts = sorted(file_content["counts"].items(), key=lambda x:x[1], reverse=True)
 
     file_content["counts"] = dict(sorted_counts)
