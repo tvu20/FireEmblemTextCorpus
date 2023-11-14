99e3fa758ca75a15107427e55f7d1434a8a9c721
Giao Vu Dinh <tvudinh20@gmail.com>
2023-11-14 15:56:21 -0500

    fe9/10 genders

--- scripts/runscripts.py
+++ scripts/runscripts.py
@@ -6,7 +6,7 @@ import sys
 
 # games = ["FE16/AzureMoon", "FE16/CrimsonFlower", "FE16/SilverSnow", "FE16/VerdantWind", "FE16/WhiteClouds"]
 
-games = ["FE13"]
+games = ["FE9", "FE10"]
 
 # games = ["FE3/BookOne", "FE3/BookTwo", "FE12/BookTwo"]
 
