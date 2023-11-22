16fdc9f02188af27a25c5c791d31f2c10c6db534
Giao Vu Dinh <tvudinh20@gmail.com>
2023-11-22 16:50:30 -0500

    more word counts

--- scripts/collect_chapters.py
+++ scripts/collect_chapters.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import sys
+import string
 
 game_path = os.path.join(os.getcwd(), "input", sys.argv[1])
 
@@ -15,3 +16,30 @@ with open(os.path.join(output_path, "full_transcript.txt"), 'w+') as file:
 
         file.write("\n")
         file.write("/***EOF***/\n")
+
+# get cleaned transcript
+
+with open(os.path.join(output_path, "full_transcript.txt"), 'r') as file, open(os.path.join(output_path, "full_transcript_cleaned.txt"), 'w+') as fo:
+    for line in file:
+        text = line.strip()
+        
+        # invalid lines
+        if len(text) == 0 or line[0] == '@' or line[0] == '%' or line[0] == '#' or line[0] == '^':
+            continue
+
+        if text == "/***EOF***/":
+            fo.write(line)
+            continue
+
+        split_speaker = text.split(": ")
+        if len(split_speaker) < 2:
+            removed_punctuation = new_line.replace("...", " ").replace("-", " ").replace("’", "").translate(str.maketrans('', '',string.punctuation)).lower().replace("…", "")
+            fo.write(removed_punctuation)
+            fo.write("\n")
+            continue
+
+        new_line = split_speaker[1:]
+        new_line = ' '.join(new_line)
+        removed_punctuation = new_line.replace("...", " ").replace("-", " ").replace("’", "").translate(str.maketrans('', '',string.punctuation)).lower().replace("…", "")
+        fo.write(removed_punctuation)
+        fo.write("\n")
--- scripts/words/words.py
+++ scripts/words/words.py
@@ -1,45 +1,35 @@
-# count number of words in a file
-# count number of unique words in a file
-# count most common words in a file
-# count words per chapter of a game
-# most common phrases
 # speaker counts
 # average sentence length
+# longest sentence by number of words
+
+
+# IN THIS FILE:
+# count number of words in a file
+# count number of unique words in a file
+# count words per chapter of a game
 # average word length
 # longest word
-# longest sentence by number of words
 # number of words only used once
+
+
+# in Google Colab Notebook
+# https://colab.research.google.com/drive/1hiv4QeqS6U7t9cye2jwDMuXvi7f3cDVv?usp=sharing
+# most common words (without stopwords)
+# most common phrases (3-6 grams)
 
 import os
 import string
+import json
 from collections import Counter
 
-game = "FE1"
+game = "FE9"
 
-transcript_path = os.path.join(os.getcwd(), "data", game, "full_transcript.txt")
-
-# delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum())
-
-# print(delchars)
-
-# words = ["ldkjflkdjfs"]
-
-# mystring = "Morzas: Basilisk! I call upon your power! Burn these arrogant humans to ash! i to you"
-
-# newstr = mystring.translate(str.maketrans('', '',string.punctuation)).lower()
-
-# for w in newstr.split():
-#     words.append(w)
-
-# print(words)
-
-# print(Counter(words))
-
-# print(mystring)
-
-# print(scrunched)
+transcript_path = os.path.join(os.getcwd(), "data", game, "full_transcript_cleaned.txt")
 
 words = []
+
+words_per_chapter = []
+current_chapter = 0
 
 with open(transcript_path,'r') as f:
 
@@ -50,19 +40,55 @@ with open(transcript_path,'r') as f:
         if len(text) == 0 or line[0] == '@' or line[0] == '%' or line[0] == '#' or line[0] == '^':
             continue
 
-        stripped = text.translate(str.maketrans('', '',string.punctuation)).lower()
+        # take care of end of chapter
+        if text == "/***EOF***/":
+            words_per_chapter.append(current_chapter)
+            current_chapter = 0
+            continue
 
-        for w in stripped.split():
+        for w in text.split():
             words.append(w)
+            current_chapter += 1
 
+        # stripped = text.translate(str.maketrans('', '',string.punctuation)).lower().replace("…", "")
 
+        # for w in stripped.split():
+        #     words.append(w)
+        #     current_chapter += 1
 
-    # words = [word for line in f
-    #          for word in line.strip().split()]
+number_of_chapters = len(words_per_chapter)
+
+print("Game: " + game)
+
+print(f"Number of chapters: {number_of_chapters}")
+print("Words per chapter:", words_per_chapter)
+print(f"Average number of words per chapter: {sum(words_per_chapter) / number_of_chapters}")
+
 print(f"Number of words: {len(words)}")
 
 words_set = Counter(words)
 
 print(f"Number of unique words: {len(words_set)}")
 
-print(words_set.most_common(20))
+words_only_appearing_once = [item for item in words_set.keys() if words_set[item] == 1]
+
+print(f"Number of words that only appear once: {len(words_only_appearing_once)}")
+
+words_least_common = words_set.most_common()[-50:]
+
+print("Sample of words used only once:", [x[0] for x in words_least_common])
+
+
+
+sortedwords = sorted(words, key=len)
+print("Longest word: %s" % (sortedwords[-5:],))
+print("Longest word length: %d" % (len(sortedwords[-1])))
+
+avg_word_length = sum(map(len, words))/len(words)
+print("Average word length: " + str(avg_word_length))
+
+
+transcript_path = os.path.join(os.getcwd(), "data", game, "full_transcript.txt")
+
+with open(os.path.join(os.getcwd(), "data", game, "words.txt"),'w+') as fo:
+    json.dump(words, fo)
