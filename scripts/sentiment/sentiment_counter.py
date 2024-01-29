e6cf186f7335641df7f130433cd6109017e1dc24
Giao Vu Dinh <tvudinh20@gmail.com>
2024-01-28 22:46:31 -0500

    sentiments

---
+++ scripts/sentiment/sentiment_counter.py
@@ -0,0 +1,33 @@
+import os
+import csv
+import json
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+data = []
+
+with open(os.path.join(__location__, "data.csv"), 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        data.append(row)
+
+# print(data[:10])
+
+file_content = {}
+
+for row in data[1:]:
+    chapter = row[1]
+    label = row[4]
+
+    if chapter not in file_content:
+        file_content[chapter] = {"positive": 0, "negative": 0, "neutral": 0}
+
+    file_content[chapter][label] += 1
+
+# print(file_content)
+
+file_content = dict(sorted(file_content.items()))
+
+with open(os.path.join(__location__, "output.json"), 'w') as fo:
+    json.dump(file_content, fo)
